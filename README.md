5168136e808ca4469623104c9429e00e4d486665
Gaby Vasconcelos <gaby@asinpa.com>
2020-09-30 23:10:15 -0500

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# merakicss-gallery
+# merakicss-billboard
+## Instructions:
+- to add a background image type background-image: url(""); under the class numSlide
+    -num is either first, second, third, etc...
+- to add a background color type background-color: name of color; under the class numSlide
+- to add height type height: ; or use one of the helpers
+- to add a width type width: ; or use one of the helpers
---
+++ script.js
@@ -0,0 +1,93 @@
+/* GALLERY */
+$(document).ready(function() {
+
+  /* slide circles */
+  $(".k-one").addClass("active");
+
+  $(".k-one").click(function() {
+    $(".slide").fadeOut();
+    $(".firstSlide").fadeIn();
+    $(".k-one").addClass("active");
+    $(".k-two").removeClass("active");
+    $(".k-three").removeClass("active");
+    $(".k-four").removeClass("active");
+    $(".k-five").removeClass("active");
+  });
+
+  $(".k-two").click(function() {
+    $(".slide").fadeOut();
+    $(".secondSlide").fadeIn();
+    $(".k-one").removeClass("active");
+    $(".k-two").addClass("active");
+    $(".k-three").removeClass("active");
+    $(".k-four").removeClass("active");
+    $(".k-five").removeClass("active");
+  });
+
+  $(".k-three").click(function() {
+    $(".slide").fadeOut();
+    $(".thirdSlide").fadeIn();
+    $(".k-one").removeClass("active");
+    $(".k-two").removeClass("active");
+    $(".k-three").addClass("active");
+    $(".k-four").removeClass("active");
+    $(".k-five").removeClass("active");
+  });
+
+  $(".k-four").click(function() {
+    $(".slide").fadeOut();
+    $(".fourthSlide").fadeIn();
+    $(".k-one").removeClass("active");
+    $(".k-two").removeClass("active");
+    $(".k-three").removeClass("active");
+    $(".k-four").addClass("active");
+    $(".k-five").removeClass("active");
+  });
+
+  $(".k-five").click(function() {
+    $(".slide").fadeOut();
+    $(".fifthSlide").fadeIn();
+    $(".k-one").removeClass("active");
+    $(".k-two").removeClass("active");
+    $(".k-three").removeClass("active");
+    $(".k-four").removeClass("active");
+    $(".k-five").addClass("active");
+  });
+
+  /* slide transition */
+  $(".firstSlide").click(function() {
+    $(".firstSlide").fadeOut();
+    $(".secondSlide").fadeIn();
+    $(".k-one").removeClass("active");
+    $(".k-two").addClass("active");
+  });
+
+  $(".secondSlide").click(function() {
+    $(".secondSlide").fadeOut();
+    $(".thirdSlide").fadeIn();
+    $(".k-two").removeClass("active");
+    $(".k-three").addClass("active");
+  });
+
+  $(".thirdSlide").click(function() {
+    $(".thirdSlide").fadeOut();
+    $(".fourthSlide").fadeIn();
+    $(".k-three").removeClass("active");
+    $(".k-four").addClass("active");
+  });
+
+  $(".fourthSlide").click(function() {
+    $(".fourthSlide").fadeOut();
+    $(".fifthSlide").fadeIn();
+    $(".k-four").removeClass("active");
+    $(".k-five").addClass("active");
+  });
+
+  $(".fifthSlide").click(function() {
+    $(".fifthSlide").fadeOut();
+    $(".firstSlide").fadeIn("slow");
+    $(".k-five").removeClass("active");
+    $(".k-one").addClass("active");
+  });
+
+});
